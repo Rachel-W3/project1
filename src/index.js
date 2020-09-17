3d74ee3c5adfc92000705e2097c897e26db7ca9f
Rachel Wong <rachelwong1f@gmail.com>
2020-09-16 22:36:09 -0400

    added init function

---
+++ src/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+(function(){
+    let canvas;
+    let ctx;
+    const canvasWidth = 640, canvasHeight = 480;
+
+    window.onload = init;
+	
+	function init(){
+        console.log("page loaded!");
+
+        canvas = document.querySelector('canvas');
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+			
+        ctx = canvas.getContext('2d');
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    }
+    
+    function setupUI(){
+
+    }
+
+    function update(){
+        
+    }
+})();
---
+++ src/rw-nsLIB.js
@@ -0,0 +1,8 @@
+"use strict";
+
+(function(){
+    // This will temporarily act as the fluid layer of the sandbox
+    function drawSineWave(){
+
+    }
+})();
